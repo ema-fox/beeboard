ba23a6f789cecb54b3fa80582594636db7d323df
Emanuel Rylke <ema@mailbox.org>
2024-04-02 17:07:04 +0200

    More desging work

--- script.js
+++ script.js
@@ -40,58 +40,111 @@ function add_today(goal) {
     }
 }
 
-function add_progress(parent, class_prefix, min, max, value, unit_text, row) {
-    let p = document.createElement('progress');
-    let pp = document.createElement('span');
-    pp.append(p);
-    pp.className = class_prefix + '-bar';
-    pp.title = value + ' ' + unit_text;
-    let number = document.createElement('span');
-    number.className = 'number ' + class_prefix + '-number';
-    number.style.gridRow = row;
-    let unit = document.createElement('span');
-    unit.className = 'unit ' + class_prefix + '-unit';
-    unit.style.gridRow = row;
-
-    if (value < max) {
-        number.innerText = Math.ceil(max - value);
-        unit.innerText = unit_text;
-    }
-    p.max = max - min;
-    p.value = value - min;
-    add_progress2(parent, class_prefix, min, max, value, unit_text, row);
-    parent.append(number, unit);
+function deltas(xs) {
+    return xs.slice(1).map((y, i) => y - xs[i]).filter(x => x);
 }
 
+function create_chunks(class_prefix, xs, divisor) {
+    let ys = deltas(xs);
+    return ys.map(y => {
+        let el = document.createElement('div');
+        el.className = `chunk ${class_prefix}`;
+        el.style.width = `${100 * y / divisor}%`;
+        return el;
+    });
+}
 
-function add_progress2(parent, class_prefix, min, max, value, unit_text, row) {
-    let bar = document.createElement('div');
-    bar.className = class_prefix + '-bar bar';
-    bar.style.gridRow = row;
-    bar.title = value + ' ' + unit_text;
-
-    progress = document.createElement('div');
-    progress.className = 'progress';
-    progress.style.width = `${100 * (value - min) / (max - min)}%`;
-    bar.append(progress);
-    parent.append(bar);
+function buf_class(x) {
+    return 'buf-' + (x < 3 ? x : x < 8 ? 3 : 8)
 }
 
 function show_progress(goal, i) {
-    let row = i + 1;
-    let goel = document.createElement('div');
-    let label = document.createElement('label');
-    label.innerText = goal.slug + '';
-    label.style.gridRow = row;
-    goel.className = 'goel ' + 'buf-' + (goal.safebuf < 3 ? goal.safebuf : goal.safebuf < 8 ? 3 : 8);
-    if (goal.week <= goal.maxrate) {
-        add_progress(goel, 'day', 0, goal.maxrate, goal.today, goal.gunits, row);
-    } else {
-        add_progress(goel, 'day', 0, goal.maxrate, goal.today, goal.gunits, row);
-        add_progress(goel, 'week', goal.maxrate, goal.week, goal.today, goal.gunits, row);
+    let days = Object.values(goal.dueby).map(x => x.delta);
+    days.push(goal.weekdue);
+    let days_adjusted = days.map(x => x + goal.today);
+
+    let prev = days_adjusted.filter(x => x <= 0);
+    let today = days_adjusted.filter(x => 0 < x && x <= goal.maxrate);
+    let next = days_adjusted.filter(x => goal.maxrate < x);
+
+    if (prev.length) {
+        prev.push(0);
     }
-    goel.append(label);
-    A.append(goel);
+
+    today.unshift(0);
+
+    if (next.length) {
+        today.push(goal.maxrate);
+        next.unshift(goal.maxrate);
+    }
+
+    let today_done = today.filter(x => x <= goal.today);
+    let today_undone = today.filter(x => goal.today < x);
+
+    if (goal.today < goal.maxrate) {
+        today_done.push(goal.today);
+        today_undone.unshift(goal.today);
+    }
+
+
+    let next_done = next.filter(x => x <= goal.today);
+    let next_undone = next.filter(x => goal.today < x);
+
+    if (goal.maxrate <= goal.today) {
+        next_done.push(goal.today);
+        next_undone.unshift(goal.today);
+    }
+
+    //console.log(goal.slug, goal.today, days, days_adjusted, deltas(prev), deltas(today_done), deltas(today_undone), deltas(next_done), deltas(next_undone));
+
+
+    let hover_text = goal.today + ' ' + goal.gunits;
+
+    let label = document.createElement('label');
+    label.innerText = goal.slug;
+
+    let prev_el = document.createElement('div');
+    prev_el.className = 'prev-bar chunk-bar';
+
+    let prev_space = document.createElement('span');
+    prev_space.className = 'space';
+    prev_el.append(prev_space);
+
+    let today_el = document.createElement('div');
+    today_el.className = 'today-bar chunk-bar ' + buf_class(goal.safebuf);
+
+    let dayduep = goal.today < goal.maxrate;
+
+    let number = document.createElement('span');
+    number.className = 'number ' + (dayduep ? 'day-due' : 'week-due');
+    number.innerText = dayduep ? Math.min(goal.maxrate - goal.today, goal.weekdue) : goal.weekdue;
+
+    today_el.append(label, number);
+
+    let next_el = document.createElement('div');
+    next_el.className = 'next-bar chunk-bar ' + buf_class(goal.safebuf);
+
+    let unit = document.createElement('span');
+    unit.className = 'unit ' + (dayduep ? 'day-due' : 'week-due');
+    unit.innerText = goal.gunits;
+
+    next_el.append(unit);
+
+    prev_el.title = hover_text;
+    today_el.title = hover_text;
+    next_el.title = hover_text;
+
+    prev_el.append(...create_chunks('prev', prev, goal.maxrate * 6));
+    today_el.append(...create_chunks('today-done', today_done, goal.maxrate),
+                    ...create_chunks('today-undone', today_undone, goal.maxrate));
+    next_el.append(...create_chunks('next-done', next_done, goal.maxrate * 6),
+                   ...create_chunks('next-undone', next_undone, goal.maxrate * 6));
+
+    let goel = document.createElement('div');
+    goel.className = 'goel ' + buf_class(goal.safebuf);
+
+    goel.append(prev_el, today_el, next_el);
+    B.append(goel);
 }
 
 let requests = 0;
@@ -133,7 +186,8 @@ async function get_goals() {
                     goal.prev_data = goal.recent_data;
                 }
             }
-            A.innerHTML = '';
+            B.innerHTML = '';
+
             response.forEach(add_today);
             response.filter(goal => goal.safebuf <= 7).toSorted((a, b) => a.fraction - b.fraction)
                 .forEach(show_progress);
